def9bc0618b0e49b5fb79b731cf8a65eb91e30c3
Brandon Laubaugh <kingkazma133@gmail.com>
2021-06-14 17:51:12 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json())
+
+//app.listen(4000, ()=>console.log ('Server running on 4000'))
+
+//app.get('/api/users/:id')
+
+app.get('/api/users', (req, res)=> {
+    let friends = ["Nit_in", "Eric", "Jeddy", "Cameron", "Riley"]
+    res.status(200).send(friends)
+})
+
+app.get("/weather/:temperature", (req, res) => {
+    const phrase = `<h3> it was ${req.params.temperature} today</h3>`
+    res.status(200).send(phrase);
+})
+app.get("/weather/:temperature", (req, res)=>{
+    const { temperature } = req.params;
+    const phrase = `<h3>it was ${temperature} today</h3>`;
+    res.status(200).send(phrase);
+}); 
+
+app.listen(4000, () => console.log("Server running on port 4000"))
+
+
